7c20f47dd7081a7c4e5b79846c05fd081697ab8b
Kavishka Fernando <kavishkafernando64@gmail.com>
2025-06-13 12:06:11 +0530

    Completed basic functionality including API intergration

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,37 @@
 // import './index.css'
 
 import {FaMapMarker, FaSearch} from "react-icons/fa";
-import sunny from './assets/images/sun.png';
 import HourlyWeather from "./components/HourlyWeather.jsx";
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
+    // Function to fetch weather data
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [city, setCity] = useState("Colombo"); // Default city
+    const [error, setError] = useState("");
+    useEffect(() => {
+        fetchWeatherData(); // fetch on initial load
+    }, []);
+    const fetchWeatherData = async () => {
+        try {
+            const response = await axios.get(`${WEATHER_API_URL}?key=${WEATHER_API_KEY}&q=${city}&days=1`);
+            setWeatherData(response.data);
+
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
+            throw error;
+        }
+    }
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            fetchWeatherData(); // Trigger search on Enter key press
+        }
+    }
+
 
     return (
         <div className="bg-gray-800 min-h-screen flex items-center justify-center ">
@@ -14,7 +41,7 @@ function App() {
                 <div className="flex">
                     <div className="flex border rounded items-center ml-6">
                         <FaSearch className="h-5 w-5"/>
-                        <input type="text" placeholder="enter city" className="pl-2 border-none focus:outline-none w-full">
+                        <input type="text" placeholder="enter city" value={city} className="pl-2 border-none focus:outline-none w-full" onChange={(e)=>setCity(e.target.value)} onKeyUp={handleKeyPress}>
 
                         </input>
                     </div>
@@ -25,18 +52,22 @@ function App() {
                     </button>
                 </div>
             {/*    weather data*/}
+                {weatherData &&
                 <div className="mt-4 text-center">
-                    <h2 className="text-xl font-semibold">Colombo</h2>
-                    <img src={sunny} alt="weather icon" className="mx-auto my-2 w-30 h-30"/>
-                    <p className="text-lg font-semibold">29 C</p>
+                    <h2 className="text-xl font-semibold">{weatherData.location.name}</h2>
+                    <img src={weatherData.current.condition.icon} alt="weather icon" className="mx-auto my-2 w-30 h-30"/>
+                    <p className="text-lg font-semibold">{weatherData.current.temp_c}C</p>
                     <p className="text-sm capitalize font-semibold">Cloudy</p>
-                    <p className="text-sm">Humidity: 70%</p>
-                    <p className="text-sm">Wind: 10 km/h</p>
-                    <p className="text-sm">UV Index: 10 km</p>
+                    <p className="text-sm">Humidity: {weatherData.current.humidity}%</p>
+                    <p className="text-sm">Wind: {weatherData.current.wind_kph} km/h</p>
+                    <p className="text-sm">UV Index: {weatherData.current.uv}</p>
 
                     </div>
+                }
                 {/*Hourly Weather*/}
-                <HourlyWeather/>
+                {weatherData?.forecast?.forecastday?.length > 0 && (
+                <HourlyWeather hourlyData={weatherData.forecast.forecastday[0].hour}/>
+                )}
             </div>
         </div>
     )
--- src/components/HourlyWeather.jsx
+++ src/components/HourlyWeather.jsx
@@ -1,21 +1,45 @@
-import React  from "react";
+import React, {useRef} from "react";
 import sunny from '../assets/images/sun.png';
+import "./HourlyWeather.css";
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
 
-function HourlyWeather(){
+function HourlyWeather({hourlyData}){
+
+    const scrollRef = useRef(null)
+    const scrollLeft = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: -200,
+                behavior: 'smooth'
+            });
+        }
+    }
+    const scrollRight = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: 200,
+                behavior: 'smooth'
+            });
+        }
+    }
     return (
-        <div className="bg-white shadow-lg mt-4 p-4 rounded w-full max-w-sm">
+        <div className="relative bg-white shadow-lg mt-4 p-4 rounded w-full max-w-sm">
             <h3 className="text-lg font-semibold mb-2">Hourly Forecast</h3>
-            <div className="flex gap-6 mx-10 py-2 overflow-x-auto scrollbar-hide " style={{scrollBehavior:'smooth'}}>
-                {Array.from({ length: 12 }, (_, index) => (
+            <div ref={scrollRef} className="flex gap-6 mx-10 py-2 overflow-x-auto scrollbar-hide " style={{scrollBehavior:'smooth'}}>
+                {hourlyData.map((hour, index) => (
                     <div key={index} className="flex flex-col items-center bg-green-100 py-2 rounded px-6 min-w-[140px] ">
-                        <p className="text-sm">{`${index + 1}:00 PM`}</p>
-                        <img src={sunny} alt="weather icon" className="w-10 h-10"/>
-                        <p className="text-sm">30°C</p>
-                        <p className="text-sm">UV index: </p>
-                        <p className="text-sm">Wind: </p>
+                        <p className="text-sm">{hour.time.split(' ')[1]}</p>
+                        <img src={hour.condition.icon || sunny} alt="weather icon" className="w-10 h-10" />
+                        <p className="text-sm">{hour.temp_c}°C</p>
+                        <p className="text-sm">UV index: {hour.uv}</p>
+                        <p className="text-sm">Wind: {hour.wind_kph} kph</p>
                     </div>
                 ))}
+
+
             </div>
+            <button onClick={scrollLeft} className="absolute left-0 top-1/2 bg-green-500  text-white transform -translate-y-1/2 rounded-full w-8 h-8 flex items-center justify-center"><FaChevronLeft className="w-4 w-4"/></button>
+            <button onClick={scrollRight} className="absolute right-0 top-1/2 bg-green-500  text-white transform -translate-y-1/2 rounded-full w-8 h-8 flex items-center justify-center"><FaChevronRight className="w-4 w-4"/></button>
         </div>
     );
 }
